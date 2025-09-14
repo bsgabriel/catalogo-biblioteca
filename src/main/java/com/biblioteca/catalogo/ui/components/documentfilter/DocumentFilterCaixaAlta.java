9ac37fb03f96cd6199b653a83db9056fb33a1674
bsgabriel <gabrielbs22@icloud.com>
2025-09-14 03:13:14 -0300

    fix: Correção espaço não funcionando em campos de texto

--- src/main/java/com/biblioteca/catalogo/ui/components/documentfilter/DocumentFilterCaixaAlta.java
+++ src/main/java/com/biblioteca/catalogo/ui/components/documentfilter/DocumentFilterCaixaAlta.java
@@ -4,19 +4,19 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DocumentFilter;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
+import static java.util.Objects.isNull;
 
 public class DocumentFilterCaixaAlta extends DocumentFilter {
 
     @Override
     public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
-        String texto = isBlank(string) ? "" : string.toUpperCase();
+        String texto = isNull(string) ? null : string.toUpperCase();
         super.insertString(fb, offset, texto, attr);
     }
 
     @Override
     public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-        String texto = isBlank(text) ? "" : text.toUpperCase();
+        String texto = isNull(text) ? null : text.toUpperCase();
         super.replace(fb, offset, length, texto, attrs);
     }
 
